9ccc6950e8e1419d53b67257f0d402c2ae5e065f
Sebastian Matias Segura <segurasebastian7887@gmail.com>
2022-08-22 18:03:44 -0300

    add comments

--- masker/masker.go
+++ masker/masker.go
@@ -45,54 +45,74 @@ func (m *masker) MaskSensitiveData(v interface{}) (interface{}, error) {
 	}
 }
 
+// maskStruct analyzes a struct and applies masking if necessary
+// it only masks fields that have been tagged as 'sensitive:"true"'
+// e.g.
+// type Example struct {
+//		A string // no masking
+//      B string `sensitive:"true"` // masking (partial)
+//      C string `sensitive:"true,full"` // masking (full)
+//}
 func (m *masker) maskStruct(s interface{}) (interface{}, error) {
 	if s == nil {
 		return nil, errors.New("cannot mask nil value")
 	}
 
-	stReflect := reflect.ValueOf(s)
 	var (
-		st   = reflect.TypeOf(s)
-		tmpr = reflect.New(st)
+		stReflect = reflect.ValueOf(s)
+		st        = reflect.TypeOf(s)
+		// create a new var with the same type of the struct
+		cloned = reflect.New(st)
 	)
 
+	// iterate over the struct fields
 	for i := 0; i < stReflect.NumField(); i++ {
-		maskTag := stReflect.Type().Field(i).Tag.Get("sensitive")
+		// get the field value
 		value := stReflect.Field(i)
+		// get the "sensitive" tag which indicates its value must be masked
+		maskTag := stReflect.Type().Field(i).Tag.Get("sensitive")
 
+		// apply different strategies depending on the field type
+		// only apply masking if the mask tag is true
 		switch value.Type().Kind() {
 		case reflect.String:
 			if maskTag != "" && maskTag != "false" {
 				tmp := m.applyMask(interfaceToString(value), maskTag)
-				tmpr.Elem().Field(i).SetString(reflect.ValueOf(tmp).String())
+				// set the masked value to the corresponding field of the cloned struct
+				cloned.Elem().Field(i).SetString(reflect.ValueOf(tmp).String())
 				continue
 			}
-			tmpr.Elem().Field(i).Set(value)
+			// set the same value to the corresponding field of the cloned struct
+			cloned.Elem().Field(i).Set(value)
 			continue
 		case reflect.Struct:
+			// if field type is struct, then make a recursive call to maskStruct
+			// and set the result to the corresponding field of the cloned struct
 			tmp, err := m.maskStruct(value.Interface())
 			if err != nil {
 				return nil, err
 			}
-			tmpr.Elem().Field(i).Set(reflect.ValueOf(tmp).Elem())
+			cloned.Elem().Field(i).Set(reflect.ValueOf(tmp).Elem())
 			continue
 		case reflect.Slice:
 			if value.IsNil() {
 				continue
 			}
+			// []string -> apply masking to every item (if necessary)
 			if value.Type().Elem().Kind() == reflect.String {
-				vals := value.Interface().([]string)
-				nv := make([]string, len(vals))
-				for i, val := range vals {
+				values := value.Interface().([]string)
+				nv := make([]string, len(values))
+				for i, val := range values {
 					if maskTag != "" && maskTag != "false" {
 						nv[i] = m.applyMask(val, maskTag)
 					} else {
 						nv[i] = val
 					}
 				}
-				tmpr.Elem().Field(i).Set(reflect.ValueOf(nv))
+				cloned.Elem().Field(i).Set(reflect.ValueOf(nv))
 				continue
 			}
+			// []struct {...} -> apply masking using recursive calls to maskStruct
 			if value.Type().Elem().Kind() == reflect.Struct {
 				nv := reflect.MakeSlice(value.Type(), 0, value.Len())
 				for j, k := 0, value.Len(); j < k; j++ {
@@ -102,18 +122,22 @@ func (m *masker) maskStruct(s interface{}) (interface{}, error) {
 					}
 					nv = reflect.Append(nv, reflect.ValueOf(tmp).Elem())
 				}
-				tmpr.Elem().Field(i).Set(nv)
+				cloned.Elem().Field(i).Set(nv)
 				continue
 			}
 		default:
-			tmpr.Elem().Field(i).Set(value)
+			// for other types nothing should be masked
+			cloned.Elem().Field(i).Set(value)
 			continue
 		}
 	}
 
-	return tmpr.Interface(), nil
+	// return
+	return cloned.Interface(), nil
 }
 
+// applyMask apply masking based on the masking tag value
+// e.g. sanitize:"true,full" -> apply full masking
 func (m *masker) applyMask(v, maskTag string) string {
 	var (
 		args      = strings.Split(maskTag, ",")
@@ -129,6 +153,8 @@ func parseMaskLevel(tag []string) int {
 	return PartialMask
 }
 
+// sanitize apply masking to a string using a provided mask character. Full-masking: replace the whole string with the
+// mask character. Partial-masking: replace the 80% of the string with the masking character.
 func sanitize(v, mask string, level int) string {
 	length := len(v)
 	if length == 0 {
